71b78740740723da18433e7d78a8e0e21c769823
JBenne98 <jgb83474@creighton.edu>
2020-02-23 19:15:06 -0600

    a

--- forms/whileAB/whileAB.js
+++ forms/whileAB/whileAB.js
@@ -1,9 +1,20 @@
-/*
+
 let firstName = prompt('Enter first name:')
 let lastName = prompt('Enter last name:')
 
-let i=0
+let firstNameChar=Array.from(firstName)
+let lastNameChar=Array.from(lastName)
 
-while() {
-  charFirstname=
-  */
+
+let counterFirst=0
+while(counterFirst<firstNameChar.length) {
+  console.log(`Letter ${counterFirst+1} is: ${firstNameChar[counterFirst]}`)
+  counterFirst++
+  }
+  
+let counterLast=0
+while(counterLast<lastNameChar.length) {
+  console.log(`Letter ${counterLast+1} is: ${lastNameChar[counterLast]}`)
+  counterLast++
+  }
+  
